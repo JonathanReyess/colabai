dd16343b532c7a1175ac1ecedaa85bff4b558cb1
Jonathan Reyes <jer88@duke.edu>
2023-11-01 16:26:56 -0400

    process student modules, title frequency in recommendaion

--- content_filtering.py
+++ content_filtering.py
@@ -14,6 +14,7 @@ tfv = TfidfVectorizer(min_df=3, max_features=None,
                       strip_accents='unicode', analyzer='word', token_pattern=r'\w{1,}', ngram_range=(1,3), stop_words= 'english')
 modules_clean_df['Description'] = modules_clean_df['Description'].fillna('')
 tfv_matrix = tfv.fit_transform(modules_clean_df['Description'])
+#print(modules_clean_df['Description'])
 #print(tfv_matrix.shape) #combination of 1017 words for our columns, this comes from the token_pattern = r\w{1,}
 sig = sigmoid_kernel(tfv_matrix, tfv_matrix) #the sigmoid library is responsible for transforming our input between 0-1, formula: 1 / 1 + e^-1
 #how is the sentence in descriptio #1 related to description #2? 
@@ -31,9 +32,45 @@ def rec(title, sig=sig):
     index = indices[title]
     sig_scores = list(enumerate(sig[index]))
     sig_scores = sorted(sig_scores, key=lambda x:x[1], reverse =True )
-    sig_scores = sig_scores[1:6]
+    sig_scores = sig_scores[1:4]
     module_indices = [i[0] for i in sig_scores]
-    return modules_clean_df['Title'].iloc[module_indices]
+    recommendation = modules_clean_df['Title'].iloc[module_indices]
+    final = []
+    for i in range(0,3):
+        temp = recommendation.index[i]
+        final.append(recommendation[temp])
+    return final
+
+#print(rec('Backing up and Sharing Code: Git'))
+
+#now that we have our recommendation function, we need to get a list of descriptions from the courses the student has taken, and pass it into
+#the recommendation engine as a temporary module. 
+
+student_clean_df = student.drop(columns=['Type', 'Hours', 'Date']) #in the future we may want to incorportate recency bias
+student_indices = pd.Series(student_clean_df.index, index=student_clean_df['Module Name'].str.strip()).drop_duplicates()
+word = "attend"
+module_list = []
+recs_list = []
+for i, row in student_clean_df.iterrows():
+    if word in row['Status']:
+        module_list.append(row['Module Name'])
+for module in module_list:
+    recs = rec(module)
+    for item in recs:
+        if item not in module_list:
+            recs_list.append(item)
+print(recs_list)
 
 
-print(rec('Wordle with Python'))
+#print(module_list)
+#description_list = []
+#for word in module_list:
+    #for i, row in modules_clean_df.iterrows():
+        #if word in row['Title']:
+            #description_list.append(row['Description'])
+#print(description_list)
+
+#p_description = " ".join(description_list)
+#print(p_description)
+
+##now what if we take this description and pass it through an LLM to summarize it?
