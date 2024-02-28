6d28cb53427d9066092b5f662a07937704b7a640
Jonathan Reyes <jer88@duke.edu>
2024-02-28 13:11:35 -0500

    deployment fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -146,34 +146,6 @@ odbc_str = (
 ';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
 )
 
-'''
-@st.cache_resource
-def init_connection():
-    return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server}" +
-        ';Server=tcp:' + st.secrets["server"] + 
-        ';PORT=1433' +
-        ';DATABASE=' + st.secrets["database"] +
-        ';Uid=' + st.secrets["username"] +
-        ';Pwd=' + st.secrets["password"] 
-
-    )
-
-conn = init_connection()
-
-@st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-
-rows = run_query("SELECT * from courses;")
-
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
-'''
-
 # Create the SQLAlchemy engine
 db_engine = create_engine(odbc_str)
 db = SQLDatabase(db_engine)
